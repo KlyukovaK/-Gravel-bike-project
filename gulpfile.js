513026bcd6bfaa32e18aff1a81bde4291a687e7a
Kristina <tinakyukova@gmail.com>
2023-04-23 17:34:21 +0300

    feat:настройка gulp

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const DIST_PATH = "./dist";
+
+function css() {
+  return gulp
+    .src("./src/sass/index.sass")
+    .pipe(sass({ includePaths: ["src/scss"] }).on("error", sass.logError))
+    .pipe(postcss([autoprefixer]))
+    .pipe(gulp.dest(`${DIST_PATH}/css`));
+}
+function html() {
+  return gulp.src("./src/*.html").pipe(gulp.dest(`${DIST_PATH}`));
+}
+
+function fonts() {
+  return gulp
+    .src("./src/vendor/fonts/**/*")
+    .pipe(gulp.dest(`${DIST_PATH}/fonts`));
+}
+function images() {
+  return gulp.src("./src/images/**/*").pipe(gulp.dest(`${DIST_PATH}/images`));
+}
+function public() {
+  return gulp.src("./src/public/**/*").pipe(gulp.dest(`${DIST_PATH}`));
+}
+
+var server = require("gulp-server-livereload");
+
+gulp.task("default", function () {
+  gulp.src("dist").pipe(
+    server({
+      livereload: true,
+      defaultFile: "index.html",
+      directoryListing: false,
+      open: true,
+    })
+  );
+});
+exports.css = css;
+exports.html = html;
+exports.assets = gulp.series(fonts, images, public);
+exports.default = gulp.series(css, html, fonts, images, public);
