d6c474da62c83affe7ebb07c0abf051673175445
Kristina <tinakyukova@gmail.com>
2023-04-30 00:11:02 +0300

    feat: реализована полностью светлая версия

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
-const server = require("gulp-server-livereload");
+const clean = require("gulp-clean");
+const uglify = require("gulp-uglify-es").default;
+const browserSync = require("browser-sync").create();
 const DIST_PATH = "./dist";
 
 function css() {
@@ -10,34 +12,56 @@ function css() {
     .src("./src/sass/index.sass")
     .pipe(sass({ includePaths: ["src/scss"] }).on("error", sass.logError))
     .pipe(postcss([autoprefixer]))
-    .pipe(gulp.dest(`${DIST_PATH}/css`));
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(gulp.dest(`${DIST_PATH}/css`))
+    .pipe(browserSync.stream());
 }
+function scripts() {
+  return gulp
+    .src(["./src/pages/index.js"])
+    .pipe(uglify())
+    .pipe(gulp.dest(`${DIST_PATH}/js`));
+}
+
 function html() {
-  return gulp.src("./src/*.html").pipe(gulp.dest(`${DIST_PATH}`));
+  return gulp
+    .src("./src/*.html")
+    .pipe(gulp.dest(`${DIST_PATH}`))
+    .pipe(browserSync.stream());
 }
 
 function fonts() {
-  return gulp
-    .src("./src/vendor/fonts/**/*")
-    .pipe(gulp.dest(`${DIST_PATH}`));
+  return gulp.src("./src/vendor/fonts/**/*").pipe(gulp.dest(`${DIST_PATH}`));
 }
 function images() {
   return gulp.src("./src/images/**/*").pipe(gulp.dest(`${DIST_PATH}/images`));
 }
-function public() {
-  return gulp.src("./src/public/**/*").pipe(gulp.dest(`${DIST_PATH}`));
-}
 
-function serv() {
-  return gulp.src(`${DIST_PATH}`).pipe(
-    server({
-      livereload: true,
-      directoryListing: false,
-      open: false,
-    })
-  );
-};
-exports.css = css;
-exports.html = html;
-exports.assets = gulp.series(fonts, images, public);
-exports.default = gulp.series(css, html, fonts, images, public, serv);
+function watching() {
+  gulp.watch(["./src/sass/index.sass"], css);
+  gulp.watch(["./src/blocks/**/*.sass"], css);
+  gulp.watch(["./src/pages/index.js"], scripts);
+  gulp.watch(["./src/images/**/*"], images);
+  gulp.watch(["./src/*.html"], html);
+}
+function browsersync() {
+  browserSync.init({
+    server: {
+      baseDir: `${DIST_PATH}`,
+    },
+  });
+}
+function cleanDist() {
+  return gulp.src("dist").pipe(clean());
+}
+// exports.assets = gulp.series(fonts, images, public);
+exports.cleanDist= cleanDist
+exports.default = gulp.parallel(
+  css,
+  html,
+  fonts,
+  images,
+  scripts,
+  browsersync,
+  watching
+);
